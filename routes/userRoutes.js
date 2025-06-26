1e8710f9563596604c3628002ddc2d84f4488881
Mauricio <mauriciogarcetee@gmail.com>
2025-06-26 19:56:21 -0300

    User route

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -4,11 +4,7 @@ const { expressjwt: checkJwt } = require("express-jwt");
 const userController = require("../controllers/userController");
 const checkUserPermissions = require("../middlewares/checkUserPermissions");
 
-router.get(
-  "/users",
-  checkJwt({ secret: process.env.JWT_SECRET, algorithms: ["HS256"] }),
-  userController.getAllUsers
-);
+router.get("/users", userController.getAllUsers);
 router.get("/users/:Id", userController.getUserById);
 router.post("/users", userController.createUser);
 router.patch(
